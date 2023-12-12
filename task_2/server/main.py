9dd52317fd95ca095f3f521aab57cee019feadad
YegorPedan <wzxv_@outlook.com>
2023-12-12 06:18:35 +0300

    create client and modify server

--- task_2/server/main.py
+++ task_2/server/main.py
@@ -2,6 +2,9 @@ import asyncio
 
 from database import ServerDatabase
 
+HOST = '127.0.0.1'
+PORT = 8888
+
 db = ServerDatabase(db_path='database.db')
 
 
@@ -9,6 +12,10 @@ async def handle_post_command(params: list):
     try:
         allocated_ram, allocated_cpus, disk_memory_size, disk_id = map(int, params)
         success = db.add_client(allocated_ram, allocated_cpus, disk_memory_size, disk_id)
+        if success:
+            return f"Client added successfully! Disk ID: {disk_id}"
+        else:
+            return f"Client with the same Disk ID {disk_id} already exists."
     except (ValueError, IndexError):
         return 'Invalid params for POST command'
 
@@ -18,32 +25,49 @@ async def process_command(command: str, params: list):
         return await handle_post_command(params)
 
 
-async def handle_client(reader, writer):
-    try:
-        while True:
-            data_str = ""
-            while True:
-                chunk = await reader.read(100)
-                if not chunk:
-                    break
-                data_str += chunk.decode()
-                if '\n' in data_str:
-                    break
+# async def handle_client(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
+#     try:
+#         while True:
+#             data_str = ""
+#             while True:
+#                 chunk = await reader.read(100)
+#                 if not chunk:
+#                     break
+#                 data_str += chunk.decode()
+#                 if '\n' in data_str:
+#                     break
+#
+#             command, *params = data_str.strip().split(" ")
+#             response = await process_command(command, params)
+#
+#             if response:
+#                 writer.write(response.encode())
+#                 await writer.drain()
+#
+#     finally:
+#         print("Closing the connection")
+#         writer.close()
+#         await writer.wait_closed()
 
-            command, *params = data_str.strip().split(" ")
-            response = await process_command(command, params)
+async def handle_client(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
+    data = None
 
-            writer.write(response.encode())
-            await writer.drain()
+    while data != b'quit':
+        data = await reader.read(256)
+        msg = data.decode()
+        addr, port = writer.get_extra_info('peername')
+        print(f'Message from {addr}:{port}: {msg!r}')
 
-    finally:
-        print("Closing the connection")
-        writer.close()
+        writer.write(data)
+        await writer.drain()
+
+    writer.close()
+    await writer.wait_closed()
 
 
 async def main():
     server = await asyncio.start_server(
-        handle_client, '127.0.0.1', 8888,
+        handle_client, HOST, PORT,
     )
     addr = server.sockets[0].getsockname()
     print(f'Server on {addr}')
@@ -53,4 +77,5 @@ async def main():
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(main())
