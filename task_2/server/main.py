4a8d768e933f9b186975bdcd9f4b0ff228ee1737
YegorPedan <wzxv_@outlook.com>
2023-12-12 04:12:27 +0300

    Delete Fast Api, use pure assuncio

--- task_2/server/main.py
+++ task_2/server/main.py
@@ -1,6 +1,30 @@
-from dotenv import load_dotenv
-from fastapi import FastAPI
+import asyncio
 
-load_dotenv()
 
-app = FastAPI(title='redsoft')
+async def handle_client(reader: str, writer: str) -> None:
+    data = await reader.read(100)
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+    print(f"Received {message!r} from {addr!r}")
+
+    response = "Hello, this is an asynchronous response!\n"
+    writer.write(response.encode())
+    await writer.drain()
+
+    print("Closing the connection")
+    writer.close()
+
+
+async def main():
+    server = await asyncio.start_server(
+        handle_client, '127.0.0.1', 8888,
+    )
+    addr = server.sockets[0].getsockname()
+    print(f'Server on {addr}')
+
+    async with server:
+        await server.serve_forever()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
